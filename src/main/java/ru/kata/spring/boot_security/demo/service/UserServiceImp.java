dfb745d807e34c96a70c900209dab85048985f54
simonychevEv <simonychev@sok-t.ru>
2024-02-16 12:37:02 +0300

    edit UserServicceImp

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -2,20 +2,15 @@ package ru.kata.spring.boot_security.demo.service;
 
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
 import javax.transaction.Transactional;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImp implements UserService {
@@ -58,13 +53,11 @@ public class UserServiceImp implements UserService {
     public User getUserByName(String name) {
         return userRepository.findByUsername(name);
     }
-
+    @Override
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
-    @Override
-    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = findByUsername(username);
         if (user == null || user.getPassword() == null) {
